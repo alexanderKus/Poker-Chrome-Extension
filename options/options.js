8dd83063874b8924b95c15f2a61161cfdea2eba3
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2024-12-27 16:51:29 +0100

    Format & Fix raises

--- options/options.js
+++ options/options.js
@@ -7,27 +7,27 @@ const factorSettings = document.getElementById("factorSettings");
 const saveBtn = document.getElementById("saveBtn");
 const defaultBtn = document.getElementById("defaultBtn");
 
-saveBtn.addEventListener("click", save)
-defaultBtn.addEventListener("click", defaultSettings)
+saveBtn.addEventListener("click", save);
+defaultBtn.addEventListener("click", defaultSettings);
 
 function save() {
-    chrome.storage.sync.set({
-        roundLength: roundLengthSettings.value,
-        smallBlind: smallBlindSettings.value,
-        bigBlind: bigBlindSettings.value,
-        maxSmallBlind: maxSmallBlindSetting.value,
-        maxBigBlind: maxBigBlindSetting.value,
-        factor: factorSettings.value
-    })
+  chrome.storage.sync.set({
+    roundLength: roundLengthSettings.value,
+    smallBlind: smallBlindSettings.value,
+    bigBlind: bigBlindSettings.value,
+    maxSmallBlind: maxSmallBlindSetting.value,
+    maxBigBlind: maxBigBlindSetting.value,
+    factor: factorSettings.value,
+  });
 }
 
 function defaultSettings() {
-    chrome.storage.sync.set({
-        roundLength: 30,
-        smallBlind: 25,
-        bigBlind:50,
-        maxSmallBlind: 1000,
-        maxBigBlind: 2000,
-        factor: 2
-    })
+  chrome.storage.sync.set({
+    roundLength: 30,
+    smallBlind: 25,
+    bigBlind: 50,
+    maxSmallBlind: 1000,
+    maxBigBlind: 2000,
+    factor: 2,
+  });
 }
--- popup/popup.js
+++ popup/popup.js
@@ -2,38 +2,65 @@ const startBtn = document.getElementById("startBtn");
 const stopBtn = document.getElementById("stopBtn");
 const resetBtn = document.getElementById("resetBtn");
 
-startBtn.addEventListener("click", start)
-stopBtn.addEventListener("click", stop)
-resetBtn.addEventListener("click", reset)
+startBtn.addEventListener("click", start);
+stopBtn.addEventListener("click", stop);
+resetBtn.addEventListener("click", reset);
 
 setInterval(updateTimer, 1000);
 
 function start() {
-    chrome.storage.sync.set({ isRunning: true })
+  chrome.storage.sync.set({ isRunning: true });
 }
 
 function stop() {
-    chrome.storage.sync.set({ isRunning: false })
+  chrome.storage.sync.set({ isRunning: false });
 }
 
 function reset() {
-    chrome.storage.sync.set({
-        timer: 0,
-        isRunning: false,
-        smallBlind: 25,
-        bigBlind: 50
-    })
+  chrome.storage.sync.set({
+    timer: 0,
+    isRunning: false,
+    smallBlind: 25,
+    bigBlind: 50,
+  });
 }
 
 async function updateTimer() {
-    const timerDiv = document.getElementById("timer");
-    const smallBlindValue = document.getElementById("smallBlindValue");
-    const bigBlindValue = document.getElementById("bigBlindValue");
-    const { smallBlind, bigBlind, timer, roundLength } =
-        await chrome.storage.sync.get(["smallBlind", "bigBlind", "timer", "roundLength"])
-    const minutes = `${roundLength - Math.ceil(timer / 60)}`.padStart(2, "0")
-    const seconds = timer % 60 == 0 ?'00' : `${60 - timer % 60}`.padEnd(2, "0")
-    timerDiv.innerText = `${minutes}:${seconds}`
-    smallBlindValue.innerText = smallBlind
-    bigBlindValue.innerHTML = bigBlind
+  const timerDiv = document.getElementById("timer");
+  const smallBlindValue = document.getElementById("smallBlindValue");
+  const bigBlindValue = document.getElementById("bigBlindValue");
+  const { smallBlind, bigBlind, timer, roundLength } =
+    await chrome.storage.sync.get([
+      "smallBlind",
+      "bigBlind",
+      "timer",
+      "roundLength",
+    ]);
+  const minutes = `${roundLength - Math.ceil(timer / 60)}`.padStart(2, "0");
+  const seconds =
+    timer % 60 == 0 ? "00" : `${60 - (timer % 60)}`.padEnd(2, "0");
+  timerDiv.innerText = `${minutes}:${seconds}`;
+  smallBlindValue.innerText = smallBlind;
+  bigBlindValue.innerHTML = bigBlind;
 }
+
+async function init() {
+  const timerDiv = document.getElementById("timer");
+  const smallBlindValue = document.getElementById("smallBlindValue");
+  const bigBlindValue = document.getElementById("bigBlindValue");
+  const { smallBlind, bigBlind, timer, roundLength } =
+    await chrome.storage.sync.get([
+      "smallBlind",
+      "bigBlind",
+      "timer",
+      "roundLength",
+    ]);
+  const minutes = `${roundLength - Math.ceil(timer / 60)}`.padStart(2, "0");
+  const seconds =
+    timer % 60 == 0 ? "00" : `${60 - (timer % 60)}`.padEnd(2, "0");
+  timerDiv.innerText = `${minutes}:${seconds}`;
+  smallBlindValue.innerText = smallBlind;
+  bigBlindValue.innerHTML = bigBlind;
+}
+
+init();
